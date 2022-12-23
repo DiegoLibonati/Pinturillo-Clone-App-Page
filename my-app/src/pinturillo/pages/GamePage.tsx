1294b5c88c6fa43ae34dc7564d9e6aed09c128ca
DiegoLibonati <diego.libonati1998@gmail.com>
2022-12-23 18:46:59 -0300

    new loader

--- my-app/src/pinturillo/pages/GamePage.tsx
+++ my-app/src/pinturillo/pages/GamePage.tsx
@@ -11,6 +11,7 @@ import { useCountdown } from "../hooks/useCountdown";
 import { setNewPoints } from "../../store/user/userSlice";
 import uuid from "react-uuid";
 import "./GamePage.css";
+import { Loader } from "../../ui/components/Loader";
 
 export const GamePage = () => {
   const [message, setMessage] = useState("");
@@ -86,118 +87,107 @@ export const GamePage = () => {
     if (round === limitRound) navigate("/");
   }, [round]);
 
-  if (countdown === 90) {
-    return (
-      <main className="main_game_container_loader">
-        <img className="loader" src={loader} alt="loader"></img>
-      </main>
-    );
-  } else {
-    return (
-      <>
-        <main className="main_game_container">
-          <section className="players_score_container">
-            {newArray.map((user) => {
+  return (
+    <>
+      {countdown === 90 && <Loader></Loader>}
+      <main className="main_game_container">
+        <section className="players_score_container">
+          {newArray.map((user) => {
+            return (
+              <article key={uuid()} className="player_score_container">
+                <div className="player_score_container_information">
+                  <h2>{user.username}</h2>
+                  <h3>{user.score?.toString()}</h3>
+                </div>
+                {(user.wasPainter || user.isPainting) && (
+                  <img className="pincel_guess" src={pincel} alt="pincel"></img>
+                )}
+                {user.guessTheWord && (
+                  <img
+                    className="image_guess"
+                    src={estrella}
+                    alt="estrella"
+                  ></img>
+                )}
+              </article>
+            );
+          })}
+        </section>
+
+        <section className="canvas_container">
+          <article className="canvas_container_title">
+            <h2>{countdown}</h2>
+            <div className="canvas_container_title_header">
+              <h3>Room: {roomId}</h3>
+              <h3>
+                Round {round}/{limitRound}
+              </h3>
+            </div>
+            <h1>{user.isPainting ? misteryWord : misteryWordToSolved}</h1>
+          </article>
+          <article className="canvas_container_toolbox">
+            {user.isPainting ? (
+              <>
+                <button id="increase" onClick={increaseSize}>
+                  +
+                </button>
+                <span id="size">{size}</span>
+                <button id="decrease" onClick={decreaseSize}>
+                  -
+                </button>
+                <input
+                  type="color"
+                  id="color"
+                  value={color}
+                  onChange={changeColor}
+                />
+                <button id="clear" onClick={clearCanvas}>
+                  B
+                </button>
+              </>
+            ) : (
+              <h3>
+                {users &&
+                  users.filter((user) => user.isPainting === true)[0]
+                    ?.username}{" "}
+                is painting
+              </h3>
+            )}
+          </article>
+          <canvas
+            id="canvas"
+            onMouseDown={user.isPainting ? startDrawing : undefined}
+            onMouseUp={user.isPainting ? finishDrawing : undefined}
+            onMouseMove={user.isPainting ? draw : undefined}
+            ref={canvasRef}
+            width="800"
+            height="781"
+          ></canvas>
+        </section>
+
+        <section className="chat_container">
+          <article className="chat_container_messages">
+            {messages.map((message) => {
               return (
-                <article key={uuid()} className="player_score_container">
-                  <div className="player_score_container_information">
-                    <h2>{user.username}</h2>
-                    <h3>{user.score?.toString()}</h3>
-                  </div>
-                  {(user.wasPainter || user.isPainting) && (
-                    <img
-                      className="pincel_guess"
-                      src={pincel}
-                      alt="pincel"
-                    ></img>
-                  )}
-                  {user.guessTheWord && (
-                    <img
-                      className="image_guess"
-                      src={estrella}
-                      alt="estrella"
-                    ></img>
-                  )}
-                </article>
+                <h2 key={uuid()}>
+                  {message.author}: {message.message}
+                </h2>
               );
             })}
-          </section>
+          </article>
 
-          <section className="canvas_container">
-            <article className="canvas_container_title">
-              <h2>{countdown}</h2>
-              <div className="canvas_container_title_header">
-                <h3>Room: {roomId}</h3>
-                <h3>
-                  Round {round}/{limitRound}
-                </h3>
-              </div>
-              <h1>{user.isPainting ? misteryWord : misteryWordToSolved}</h1>
-            </article>
-            <article className="canvas_container_toolbox">
-              {user.isPainting ? (
-                <>
-                  <button id="increase" onClick={increaseSize}>
-                    +
-                  </button>
-                  <span id="size">{size}</span>
-                  <button id="decrease" onClick={decreaseSize}>
-                    -
-                  </button>
-                  <input
-                    type="color"
-                    id="color"
-                    value={color}
-                    onChange={changeColor}
-                  />
-                  <button id="clear" onClick={clearCanvas}>
-                    B
-                  </button>
-                </>
-              ) : (
-                <h3>
-                  {users &&
-                    users.filter((user) => user.isPainting === true)[0]
-                      ?.username}{" "}
-                  is painting
-                </h3>
-              )}
-            </article>
-            <canvas
-              id="canvas"
-              onMouseDown={user.isPainting ? startDrawing : undefined}
-              onMouseUp={user.isPainting ? finishDrawing : undefined}
-              onMouseMove={user.isPainting ? draw : undefined}
-              ref={canvasRef}
-              width="800"
-              height="781"
-            ></canvas>
-          </section>
-
-          <section className="chat_container">
-            <article className="chat_container_messages">
-              {messages.map((message) => {
-                return (
-                  <h2 key={uuid()}>
-                    {message.author}: {message.message}
-                  </h2>
-                );
-              })}
-            </article>
-
-            {!user.guessTheWord && !user.isPainting && (
-              <form className="chat_container_input" onSubmit={sendMessage}>
-                <input
-                  type="text"
-                  placeholder="Write..."
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                ></input>
-              </form>
-            )}
-          </section>
-        </main>
-      </>
-    );
-  }
+          {!user.guessTheWord && !user.isPainting && (
+            <form className="chat_container_input" onSubmit={sendMessage}>
+              <input
+                type="text"
+                placeholder="Write..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></input>
+            </form>
+          )}
+        </section>
+      </main>
+    </>
+  );
 };
