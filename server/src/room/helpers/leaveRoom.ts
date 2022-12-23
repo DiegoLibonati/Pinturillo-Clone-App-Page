1cb5786695c01972fbd7d0e5bc19fedca38a7cea
DiegoLibonati <diego.libonati1998@gmail.com>
2022-12-23 15:41:02 -0300

    leaveRoom refactor

--- server/src/room/helpers/leaveRoom.ts
+++ server/src/room/helpers/leaveRoom.ts
@@ -1,20 +1,23 @@
 import { rooms } from "..";
 
 export const leaveRoom = (roomId, user, socket) => {
-  if (rooms[roomId]) {
+  const room = rooms[roomId];
+  const roomParticipants = rooms[roomId].participants;
+
+  if (room) {
     const userId = user.userId;
 
-    rooms[roomId].participants = rooms[roomId].participants.filter(
+    rooms[roomId].participants = roomParticipants.filter(
       (room) => room.userId !== userId
     );
 
-    if (user.isOwner && rooms[roomId][0]) {
-      rooms[roomId].participants[0]["isOwner"] = true;
+    if (user.isOwner && roomParticipants[0]) {
+      roomParticipants[0]["isOwner"] = true;
     }
 
     socket.to(roomId).emit("user-disconnect", { user });
 
-    if (rooms[roomId].participants.length === 0) {
+    if (roomParticipants.length === 0) {
       delete rooms[roomId];
     }
   }
