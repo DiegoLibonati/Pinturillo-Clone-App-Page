76a7dc3d8ed3f34ab20366d3d240787ee9bb5293
DiegoLibonati <diego.libonati1998@gmail.com>
2023-01-07 12:00:14 -0300

    Validation if room exist or not and totalRoundsKey

--- server/src/room/helpers/createRoom.ts
+++ server/src/room/helpers/createRoom.ts
@@ -1,19 +1,18 @@
 import { Socket } from "socket.io";
 import { rooms } from "..";
 export const createRoom = (roomId: string, socket: Socket): void => {
-  const room = rooms[roomId];
+  if (rooms[roomId]) return;
 
-  if (!room) {
-    rooms[roomId] = {
-      participants: [],
-      countdown: 90,
-      newPainterSetted: false,
-      userPainting: null,
-      userWasPainterId: null,
-      userWasAPainter: null,
-      roomIsStarted: false,
-    };
-    socket.emit("room-created", { roomId });
-    console.log(`ROOM CREATED: ${roomId}`);
-  }
+  rooms[roomId] = {
+    participants: [],
+    countdown: 90,
+    newPainterSetted: false,
+    userPainting: null,
+    userWasPainterId: null,
+    userWasAPainter: null,
+    roomIsStarted: false,
+    totalRounds: 3,
+  };
+  socket.emit("room-created", { roomId });
+  console.log(`ROOM CREATED: ${roomId}`);
 };
