765655fbd2a630ec0e1dfd211c47e6a98fa6f3c5
DiegoLibonati <diego.libonati1998@gmail.com>
2023-01-07 11:59:54 -0300

    Validation if room exist or not and totalRoundsKey

--- server/src/room/helpers/deleteRoom.ts
+++ server/src/room/helpers/deleteRoom.ts
@@ -1,16 +1,17 @@
 import { rooms } from "..";
 
 export const deleteRoom = (roomId: string): void => {
-  if (rooms[roomId]) {
-    rooms[roomId] = {
-      participants: [],
-      countdown: 90,
-      newPainterSetted: false,
-      userPainting: null,
-      userWasAPainter: null,
-      userWasPainterId: null,
-      roomIsStarted: false,
-    };
-    delete rooms[roomId];
-  }
+  if (!rooms[roomId]) return;
+
+  rooms[roomId] = {
+    participants: [],
+    countdown: 90,
+    newPainterSetted: false,
+    userPainting: null,
+    userWasAPainter: null,
+    userWasPainterId: null,
+    roomIsStarted: false,
+    totalRounds: 3,
+  };
+  delete rooms[roomId];
 };
